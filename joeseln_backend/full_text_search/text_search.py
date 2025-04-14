550925b5ecd3eb2fdb1893c2efa126f06d2ab025
joe baudisch <josef.baudisch@frm2.tum.de>
2025-04-14 11:02:10 +0200

    text search bug
    Change-Id: I5e1a1f7f98fd21165901562a1e64be835cab6d1b

--- joeseln_backend/full_text_search/text_search.py
+++ joeseln_backend/full_text_search/text_search.py
@@ -1,10 +1,11 @@
 from sqlalchemy import or_
 from typesense.client import Client
 from joeseln_backend.models import models
-from joeseln_backend.services.labbook.labbook_service import \
-    check_for_labbook_access
 from joeseln_backend.conf.base_conf import LABBOOK_QUERY_MODE
-from joeseln_backend.services.user_to_group.user_to_group_service import get_user_groups
+from joeseln_backend.services.user_to_group.user_to_group_service import \
+    get_user_groups
+
+
 def search_with_model(db, model, search_text, user, typesense: Client):
     result_array = []
     # query for labbook_id that user has access right
@@ -13,33 +14,38 @@ def search_with_model(db, model, search_text, user, typesense: Client):
     if LABBOOK_QUERY_MODE == 'match':
         lbs = db.query(models.Labbook).filter(
             or_(*[models.Labbook.title.contains(name) for name in
-            user_groups])).all()
+                  user_groups])).all()
     else:
         lbs = db.query(models.Labbook).filter(
-        models.Labbook.title.in_(user_groups)).all()
+            models.Labbook.title.in_(user_groups)).all()
     labbook_ids = [str(lb.id) for lb in lbs]
-    
+
     if 'note' in model:
         search_parameters = {
             'q': search_text,
-            'filter_by': f"labbook_id:[{','.join(labbook_ids)}] && soft_delete:=false",  # filter for notebook that user can access
+            'filter_by': f"labbook_id:[{','.join(labbook_ids)}] && soft_delete:=false",
+            # filter for notebook that user can access
             'query_by': 'subject,content',
             'fuzzy': True
         }
-        
-        search_res = typesense.collections['notes'].documents.search(search_parameters)['hits']
+
+        search_res = \
+        typesense.collections['notes'].documents.search(search_parameters)[
+            'hits']
         for result in search_res:
             result = result["document"]
-            lb_elem = db.query(models.Labbookchildelement).get(result["elem_id"])
-            created_by = db.query(models.User).get(lb_elem.created_by_id)
-            res_dic = {'content_type_model': 'shared_elements.note',
+            lb_elem = db.query(models.Labbookchildelement).get(
+                result["elem_id"])
+            if lb_elem:
+                created_by = db.query(models.User).get(lb_elem.created_by_id)
+                res_dic = {'content_type_model': 'shared_elements.note',
                            'display': result["subject"],
                            'created_by': created_by,
                            'pk': str(lb_elem.labbook_id),
                            'labbook_pos_y': lb_elem.position_y,
                            'element_pk': str(result["id"])}
-            result_array.append(res_dic)
- 
+                result_array.append(res_dic)
+
         # subject content
     if 'file' in model:
         results = db.query(models.File).filter(
@@ -47,7 +53,7 @@ def search_with_model(db, model, search_text, user, typesense: Client):
                 models.File.description.like(f'%{search_text}%'))).all()
         for result in results:
             lb_elem = db.query(models.Labbookchildelement).get(result.elem_id)
-            if str(lb_elem.labbook_id) in labbook_ids:
+            if lb_elem and str(lb_elem.labbook_id) in labbook_ids:
                 created_by = db.query(models.User).get(
                     lb_elem.created_by_id)
                 res_dic = {'content_type_model': 'shared_elements.file',
@@ -63,7 +69,7 @@ def search_with_model(db, model, search_text, user, typesense: Client):
             or_(models.Picture.title.like(f'%{search_text}%'))).all()
         for result in results:
             lb_elem = db.query(models.Labbookchildelement).get(result.elem_id)
-            if str(lb_elem.labbook_id) in labbook_ids:
+            if lb_elem and str(lb_elem.labbook_id) in labbook_ids:
                 created_by = db.query(models.User).get(
                     lb_elem.created_by_id)
                 res_dic = {'content_type_model': 'pictures.picture',
