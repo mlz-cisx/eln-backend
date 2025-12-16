e1872211df652a2a33e542053da32f61cd01ba1b
joe baudisch <josef.baudisch@frm2.tum.de>
2025-12-16 17:27:35 +0100

    fix migration script
    Change-Id: Icfc90dd4bb850bd5d4a67bf7b4e30eb9926880c8
    Reviewed-on: https://forge.frm2.tum.de/review/c/mlz/eln/backend/+/38137
    Reviewed-by: Wojian Wang <wojian.wang@frm2.tum.de>
    Tested-by: Josef Baudisch <josef.baudisch@frm2.tum.de>

--- joeseln_backend/database/migration_scripts.py
+++ joeseln_backend/database/migration_scripts.py
@@ -14,7 +14,31 @@ def update_db_tables():
         )
         connection.execute(
             text(
-                "ALTER TABLE public.picture ADD IF NOT EXISTS canvas_content  TEXT  DEFAULT '';"
+                """
+                ALTER TABLE public.picture 
+                   ADD COLUMN IF NOT EXISTS canvas_content TEXT 
+                   DEFAULT '{"version":"6.9.0","objects":[],"background":"#F8F8FF"}';
+                   """
+            )
+        )
+        connection.execute(
+            text(
+                """
+                DO $$
+                BEGIN
+                    IF EXISTS (
+                        SELECT 1
+                        FROM information_schema.columns
+                        WHERE table_schema = 'public'
+                          AND table_name = 'picture'
+                          AND column_name = 'canvas_content'
+                    ) THEN
+                        UPDATE public.picture
+                        SET canvas_content = '{"version":"6.9.0","objects":[],"background":"#F8F8FF"}'
+                        WHERE canvas_content = '';
+                    END IF;
+                END$$;
+                """
             )
         )
         transaction.commit()
